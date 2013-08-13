a22b0d5947c7062ca91b7838e1b473cef391e268
Mannybitcoins <mannybitcoins@gmail.com>
2013-08-12 23:21:44 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MannyApp::Application.config.secret_key_base = '0b82a313003e61b9473bbd5c88bf8dddbcbb320a8ad2cf741be1d75c145aab825fe369b45313946ae91e5487723d58e9908218209e6cfcd24b0b39c751858959'
